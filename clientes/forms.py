c7a69c6f5244af9284c10c81a65720827f70d4e7
Felipe Alvarez dos Santos <alvarezfelipe_@hotmail.com>
2024-04-14 11:52:29 -0300

    CRUD Pais

--- clientes/forms.py
+++ clientes/forms.py
@@ -63,3 +63,76 @@ class CadastroCriancaForm(forms.ModelForm):
         if Crianca.objects.filter(crianca_nome=valida_crianca_nome, 
                                   crianca_responsavel=valida_crianca_responsavel).exists():
             raise ValidationError(f"A criança {valida_crianca_nome} já existe para o responsável {valida_crianca_responsavel}")
+
+#Form para Pai/responsável
+class CadastroPaiForm(forms.ModelForm):
+    class Meta:
+        model = Pais
+        fields = [
+            'pai_nome',
+            'pai_rg',
+            'pai_cpf',
+            'pai_celular',
+            'pai_cep',
+            'pai_endereco',
+            'pai_num_casa',
+            'pai_complemento_casa',
+            'pai_bairro',
+            'pai_cidade',
+            'pai_uf',
+            'pai_email',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'visualizar_resp'
+        self.helper.add_input(
+            Submit(
+                "submit",
+                "Salvar",
+                css_class="btn btn-dark"
+            )
+        )
+
+        self.helper.add_input(
+            Button(
+                "voltar",
+                "Voltar",
+                css_class= "btn btn-outline-dark",
+                onclick = "javascript:history.back()",
+            )
+        )
+
+        self.helper.layout = Layout(
+            Fieldset(
+                "",
+                "pai_nome",
+                Div(
+                    Field("pai_rg", wrapper_class="col"),
+                    Field("pai_cpf", wrapper_class="col"),                    
+                    Field("pai_celular", wrapper_class="col"),                    
+                    Field("pai_email", wrapper_class="col"),                    
+
+                    css_class= "row",
+                ),
+                Div(
+                    Field("pai_cep", wrapper_class="col"),
+                    Field("pai_endereco", wrapper_class="col"),                    
+                    Field("pai_num_casa", wrapper_class="col"),                    
+                    Field("pai_complemento_casa", wrapper_class="col"),                    
+                    
+                    css_class= "row",
+                ),
+                Div(
+                    Field("pai_bairro", wrapper_class="col"),                    
+                    Field("pai_cidade", wrapper_class="col"),                    
+                    Field("pai_uf", wrapper_class="col"),                    
+
+                    css_class= "row",
+                ),
+                css_class= "border-bottom mb-3"
+                )
+            
+        )

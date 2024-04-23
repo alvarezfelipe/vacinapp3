fa67a2c9b4e6bfe0acfb942df372679606564729
Felipe Alvarez dos Santos <alvarezfelipe_@hotmail.com>
2024-04-23 01:32:24 -0300

    create calendario_vacina table

--- vacina/admin.py
+++ vacina/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Vacina
+from .models import *
 
 # Register your models here.
 class VacinaAdmin(admin.ModelAdmin):
     list_display = ('vacina_nome','vacina_lote','vacina_fabricante')
     ordering = ('vacina_nome',)
 
+class CalendarioAdmin(admin.ModelAdmin):
+    list_display = ('calendario_vacina','calendario_dose','calendario_idade')
+    ordering = ('calendario_vacina',)
+
 admin.site.register(Vacina, VacinaAdmin)
+admin.site.register(CalendarioVacina, CalendarioAdmin)
--- vacina/models.py
+++ vacina/models.py
@@ -3,8 +3,23 @@ from django.db import models
 # Create your models here.
 class Vacina(models.Model):
     vacina_nome = models.CharField('Vacina', max_length=250)
-    vacina_lote = models.CharField('Lote', max_length=250)
-    vacina_fabricante = models.CharField('Lote', max_length=250)
+    vacina_lote = models.CharField('Lote', max_length=250, blank=True)
+    vacina_fabricante = models.CharField('Lote', max_length=250, blank=True)
 
     def __str__(self):
         return self.vacina_nome
+    
+class CalendarioVacina(models.Model):
+    dose_choices = [
+        ('1ª dose', '1ª dose'), 
+        ('2ª dose', '2ª dose'), 
+        ('3ª dose', '3ª dose'), 
+        ('4ª dose', '4ª dose'), 
+        ('6ª dose', '6ª dose'), 
+        ('dose unica', 'dose unica'), 
+        ('reforço', 'reforço'), 
+    ]
+
+    calendario_vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    calendario_dose = models.CharField('Dose', max_length=20, choices=dose_choices)
+    calendario_idade = models.CharField('Idade', max_length=20)
